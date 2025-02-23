fc7d8232c555e9c404c3b478388594a45cf1125e
kake-t <36560265+kake-t@users.noreply.github.com>
2025-02-23 08:09:02 +0000

    型を追加

--- src/infrastructure/lambda_handler.ts
+++ src/infrastructure/lambda_handler.ts
@@ -1,9 +1,19 @@
-export const handler = async (event: any) => {
-    // TODO implement
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
-  };
-  
+import type { Context } from "aws-lambda";
+
+interface EventHandler {
+	prompt: string;
+}
+
+interface EventHandlerResponse {
+	statusCode: number;
+	body: string;
+}
+
+export const handler = async (event: EventHandler, context: Context): Promise<EventHandlerResponse> => {
+	// TODO implement
+	const response: EventHandlerResponse = {
+		statusCode: 200,
+		body: JSON.stringify("Hello from Lambda!"),
+	};
+	return response;
+};
