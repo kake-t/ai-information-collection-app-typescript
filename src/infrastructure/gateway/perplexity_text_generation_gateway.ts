0c0cd1fef554903029ef55bc37626810886196cb
kake-t <36560265+kake-t@users.noreply.github.com>
2025-03-01 02:01:36 +0000

    apiエラーメッセージ修正

--- src/infrastructure/gateway/perplexity_text_generation_gateway.ts
+++ src/infrastructure/gateway/perplexity_text_generation_gateway.ts
@@ -32,6 +32,14 @@ interface PerplexityResponse {
 	};
 }
 
+interface PerplexityError {
+	detail: {
+		loc: string[];
+		msg: string;
+		type: string;
+	}[];
+}
+
 /**
  * 生成された文章が存在しないエラー
  * @extends Error
@@ -58,8 +66,12 @@ class UsageInformationIsNoneError extends Error {
  * @extends Error
  */
 class PerplexityAPIError extends Error {
-	constructor() {
-		super("Perplexity API error: Response is failed");
+	constructor(statusCode: number, errorResponse: PerplexityError | null = null) {
+		let message = `Perplexity API error: Response is failed. status code=${statusCode}`;
+		if (errorResponse) {
+			message += `, response=${JSON.stringify(errorResponse)}`;
+		}
+		super(message);
 		this.name = "PerplexityAPIError";
 	}
 }
@@ -93,7 +105,10 @@ export class PerplexityTextGenerationGateway implements TextGenerationGateway {
 			}),
 		});
 		if (!response.ok) {
-			throw new PerplexityAPIError();
+			if (response.status === 422) {
+				throw new PerplexityAPIError(response.status, (await response.json()) as PerplexityError);
+			}
+			throw new PerplexityAPIError(response.status);
 		}
 		const data = (await response.json()) as PerplexityResponse;
 
