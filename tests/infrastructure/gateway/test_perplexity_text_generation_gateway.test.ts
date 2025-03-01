735b5ab2b8534e3e60dbd615c14369ca8100bf8e
kake-t <36560265+kake-t@users.noreply.github.com>
2025-03-01 03:31:10 +0000

    テスト実行時に環境変数を読み込む

--- tests/infrastructure/gateway/test_perplexity_text_generation_gateway.test.ts
+++ tests/infrastructure/gateway/test_perplexity_text_generation_gateway.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, jest, test } from "@jest/globals";
+import * as dotenv from "dotenv";
 import { TextGenerationRequest } from "../../../src/domain/entities/text_generation";
 import { PerplexityTextGenerationGateway } from "../../../src/infrastructure/gateway/perplexity_text_generation_gateway";
+dotenv.config();
 
 describe("PerplexityTextGenerationGateway", () => {
 	test("generate text is success", async () => {
